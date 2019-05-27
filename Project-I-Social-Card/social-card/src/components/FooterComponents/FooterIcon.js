8d7d538cd342986462a84e3cc7a6e4cd0d4ab77f
Josemaria <josemarianriagu25@gmail.com>
2019-05-28 05:11:35 +0800

    Code fixes and cleanup Stretch completed

--- Project-I-Social-Card/social-card/src/components/FooterComponents/FooterIcon.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/FooterIcon.js
@@ -3,8 +3,9 @@ import './Footer.css';
 
 const anchorclick = (e) => {
     e.preventDefault();
-    window.alert('You clicked an icon');
+    window.alert('You clicked an icon!');
 }
+
 const FooterIcon = ({ src, count }) => {
     return (
         <div className='footer-icon' onClick={anchorclick}>

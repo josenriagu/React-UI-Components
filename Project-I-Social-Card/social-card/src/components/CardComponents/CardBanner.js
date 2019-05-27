ccd9fdbc32909ee602f476768da26ebec4476331
Josemaria <josemarianriagu25@gmail.com>
2019-05-27 21:41:01 +0800

    Build CardBanner component

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,2 +1,17 @@
 import React from 'react';
 import './Card.css';
+
+const bannerattribute = {
+    src: "https://tk-assets.lambdaschool.com/fcd75197-7d12-46ec-bc9e-4130f34822fa_reactbackground.png",
+    alt: "react-logo-base"
+}
+
+const CardBanner = () => {
+    return (
+        <div className='card-banner'>
+            <img src={bannerattribute.src} alt={bannerattribute.alt} />
+        </div>
+    );
+}
+
+export default CardBanner;

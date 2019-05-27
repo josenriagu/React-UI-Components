d35688d03012290ab6c967d1e60a44a902f269c3
Josemaria <josemarianriagu25@gmail.com>
2019-05-27 21:39:47 +0800

    Build HeaderTitle component

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,22 @@
 import React from 'react';
 import './Header.css';
+
+const title = {
+    h3: "Lambda School",
+    handle: "@LambdaSchool",
+    separator : ".",
+    date: "26 Jan"
+}
+
+const HeaderTitle = () => {
+    return(
+        <div className='header-title'>
+            <h3>{title.h3}</h3>
+            <span>{title.handle}</span>
+            <span>{title.separator}</span>
+            <span>{title.date}</span>
+        </div>
+    );
+}
+
+export default HeaderTitle;

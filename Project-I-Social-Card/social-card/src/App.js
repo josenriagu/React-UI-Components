b76d74e6792802ec12bf8c2ca48320236e224a77
Josemaria <josemarianriagu25@gmail.com>
2019-05-28 01:09:39 +0800

    Import FooterContainer into App

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
 import HeaderContainer from './components/HeaderComponents/HeaderContainer';
-import CardContainer from './components/CardComponents//CardContainer';
+import CardContainer from './components/CardComponents/CardContainer';
+import FooterContainer from './components/FooterComponents/FooterContainer';
 
 const App = () => {
   return (
     <div className='app'>
       <HeaderContainer />
       <CardContainer />
+      <FooterContainer />
     </div>
     // <div>
     //   <h3>Welcome to React Social Card!</h3>
